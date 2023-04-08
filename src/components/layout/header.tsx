55627cbeeadad2d64a9eaf960c8dc039ac0a31fd
JSExpert1121 <jingang0314@gmail.com>
2023-04-08 10:33:26 -0300

    feat: header component finished

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,5 +1,19 @@
+import { iconHome, iconPerson, iconEmoji, iconDiamond } from 'assets/icons';
+import { IconButton } from 'components/base/button';
+import { Icon } from 'components/base/icon';
+
+const HEADER_ICONS = [
+    { id: 1, content: iconHome },
+    { id: 2, content: iconPerson },
+    { id: 3, content: iconEmoji },
+    { id: 4, content: iconDiamond },
+]
 export const Header = () => (
     <header>
-        Header
+        {HEADER_ICONS.map((icon) => (
+            <IconButton>
+                <Icon key={icon.id} content={icon.content} />
+            </IconButton>
+        ))}
     </header>
 );
