06db8b13cd29b5795212541688cb7511762ff541
JSExpert1121 <jingang0314@gmail.com>
2023-04-10 04:43:33 -0300

    feat: top-bar

--- src/pages/collections/collections.tsx
+++ src/pages/collections/collections.tsx
@@ -1,25 +1,29 @@
 import { useQuery } from 'react-query';
+import { getCollections } from 'service';
+import { CollectionType } from 'types';
+import { CollectionCard } from './components';
+import { StatCard } from './components/stat-card';
+import './styles.scss';
 
 export function Collections() {
+    const {
+        data: collections,
+        isLoading: collectionsFetching,
+        error: collectionsError
+    } = useQuery<CollectionType[], any>('collections', getCollections);
 
-    // Fetch collection data (response will be mocked)
-    const fetchCollections = async () => {
-        const res = await fetch('http://mock-server/collections');
-        return res.json();
-    };
-    const collection = useQuery('collections', fetchCollections);
-
-    // Collection data will be accessible 
-    // here, using the mock server.
-    // To manipulate this reponse object,
-    // change ./src/mocks/handlers/collection.ts
-    console.log('#############');
-    console.log(collection.data);
-    console.log('#############');
-
+    console.log(collections, collectionsFetching, collectionsError);
     return (
-        <section className='container container-md'>
-            Collections page
+        <section className='container container-lg'>
+            <section className='top-bar'>
+                <CollectionCard />
+                <section className='stats'>
+                    <StatCard label='ASSETS' value={10000} />
+                    <StatCard label='HOLDERS' value={5257} />
+                    <StatCard label='VOLUME' value={98561} />
+                    <StatCard label='FLOOR PRICE' value={0.515} />
+                </section>
+            </section>
         </section>
     );
 }
